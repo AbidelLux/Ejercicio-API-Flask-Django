f80128c2dcb5601b8738b4ccfe6bcdb0930294f2
Hilary Rocio Lux <3031591380108@ingenieria.usac.edu.gt>
2022-05-04 01:05:41 -0600

    metodo post en para agregar y delete para personajes

--- backend/main.py
+++ backend/main.py
@@ -11,6 +11,23 @@ manager = Manager()
 def prueba():
     return "hola a todos soy una api, segunda frase"
 
+@app.route('/delete/<string:name>',methods=['DELETE'])
+def delete_character(name):
+    if request.method == 'DELETE':
+        if name is not None:
+            ok =  manager.delete_character(name)
+            if ok:
+                return jsonify({'ok':ok,'msg':'personaje eliminado'}),200
+            else:
+                return jsonify({'ok':False,'msg':'no se pudo eliminar'}),404
+        return jsonify({'ok': False, 'msg': 'Solicitu incompleta'}), 500
+    return jsonify({'msg':'metodo no permitido'}),405
+
+@app.route('/add',methods=['POST'])
+def add():
+    json = request.get_json()
+    manager.add_character(json['name'],json['anime'],json['image'],json['desc'])
+    return jsonify({'ok':True,'msg':'personaje agregado con exito'}),200
 @app.route('/addVarios',methods=['POST'])
 def add_character():
     print(request.data)
--- frontend/app/views.py
+++ frontend/app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from app.forms import  FileForm
+from app.forms import  FileForm,AddForm,DeleteForm
 import requests
 # Create your views here.
 
@@ -18,10 +18,26 @@ def home(request):
     return render(request,'index.html',context)
 
 def add(request):
-    pass
+    if request.method == 'POST':
+        form = AddForm(request.POST)
+        if form.is_valid():
+            json_data = form.cleaned_data
+            response = requests.post(endpoint + 'add',json = json_data)
+
+            if response.ok:
+                return  render(request,'add.html',{'form':form})
+        return render(request, 'add.html', {'form': form})
+    return render(request,'add.html')
 
 def delete(request):
-    pass
+    if request.method == 'POST':
+        form = DeleteForm(request.POST)
+        if form.is_valid():
+            json_data = form.cleaned_data
+            response =  requests.delete(endpoint+'delete/'+json_data['name'])
+    else:
+        form = DeleteForm()
+    return render(request,'delete.html',{'form':form})
 
 def cargaMasiva(request):
     ctx = {
@@ -34,8 +50,15 @@ def cargaMasiva(request):
             f = request.FILES['file']
             print(f.name)
             xml_binary = f.read()
+            xml = xml_binary.decode('utf-8')
+            ctx['content'] = xml
             reponse = requests.post(endpoint + 'addVarios',data=xml_binary)
+            print(reponse)
             if reponse.ok:
                 ctx['response'] = 'archivo cargado correcto'
+            else:
+                ctx['response'] = 'se encontro un error en el archivo'
+        else:
+            return render(request,'carga.html')
     return render(request,'carga.html',ctx)
 
